f95864893f023e8969063ca32ad6214aafbf4272
jtb75-wiz <153871848+jtb75-wiz@users.noreply.github.com>
2023-12-15 08:00:00 -0600

    Update c5-04-securitygroup-privatesg.tf

--- terraform-manifests/c5-04-securitygroup-privatesg.tf
+++ terraform-manifests/c5-04-securitygroup-privatesg.tf
@@ -9,7 +9,8 @@ module "private_sg" {
   vpc_id = module.vpc.vpc_id
   # Ingress Rules & CIDR Blocks
   ingress_rules = ["ssh-tcp", "http-80-tcp"]
-  ingress_cidr_blocks = [module.vpc.vpc_cidr_block]
+  ##ingress_cidr_blocks = [module.vpc.vpc_cidr_block]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
   # Egress Rule - all-all open
   egress_rules = ["all-all"]
   tags = local.common_tags
